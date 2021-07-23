a3a753bf6e13c7dc3b0b4010edbf198250e88132
mousieee1011 <ezra_k@naver.com>
2021-07-23 17:31:33 +0900

    first commit

---
+++ src/firstProject/HelloWorld.java
@@ -0,0 +1,17 @@
+package firstProject;// 코드의 그룹을 지을대 사용!
+					//한줄석
+				    /* 여러줄 주석
+				      * 
+				      * 
+				      * 
+				      * 
+				      * */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+		System.out.println("안녕하세요. 올해 " + 19 + 1 + "살이 된 " + '김' + "자바입니다.");
+	
+	}
+}
+	
